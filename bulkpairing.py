a9ba615108548588084fd64bfa346f423bd42ea1
jplight <janpfister@t-online.de>
2021-08-19 18:15:57 +0200

    add create_bulk()

--- bulkpairing.py
+++ bulkpairing.py
@@ -2,6 +2,7 @@ import requests
 import time
 import json
 import config
+import sys
 
 from datetime import datetime
 from typing import Dict, List, Optional, Tuple, Iterator, Any
@@ -31,11 +32,11 @@ http = requests.Session()
 def get_tokens(pair, candidates):
     t1, t2 = None, None
     for c in candidates["candidates"]:
-        if c["order"] == pair[0]:
+        if c["player_id"] == pair[0]:
             t1 = c["token"]
             break
     for c in candidates["candidates"]:
-        if c["order"] == pair[1]:
+        if c["player_id"] == pair[1]:
             t2 = c["token"]
             break
     return t1, t2
@@ -53,12 +54,16 @@ def create_pairings(pairings):
     return players
 
 
-def create_games_bulk(pairings, clock_limit, clock_incr, waiting):
+def create_games_bulk(pairings, clock_limit, clock_incr, pair_in, message, rated):
     pairings = create_pairings(pairings=pairings)
     now = int(time.time()) * 1000
     data = GAME_SETTINGS.copy()
     data["players"] = ",".join(pairings)
-    data["pairAt"] = now + 1000 * 60 * 1
+    data["pairAt"] = now + 1000 * 60 * pair_in
+    data["clock.limit"] = clock_limit
+    data["clock.increment"] = clock_incr
+    data["message"] = message
+    data["rated"] = rated
     rep = http.post(BULK_CREATE_API, data=data, headers=HEADERS)
     print(rep.text)
     log = open("log.json", "a")
@@ -68,13 +73,47 @@ def create_games_bulk(pairings, clock_limit, clock_incr, waiting):
     json_string = json.loads(rep.text)
     json.dump(json_string, log, indent=3)
     log.close()
+    return rep
 
 
-def create_games_initial(clock_limit, clock_incr):
-
-
-
-    create_games_bulk(pairings=, clock_limit=clock_limit, )
+def create_bulk():
+    count = 0
+    pairings_amount = int(input("How many Pairings you want to create?"))
+    pairings = []
+    while count < pairings_amount:
+        player1 = input(f"ID of the player with white for the {count+1} pairing:")
+        player2 = input(f"ID of the player with black for the {count+1} pairing:")
+        pairing = [player1, player2]
+        pairings.append(pairing)
+        count += 1
+    clock_limit = str(input("Clock Limit (in seconds):"))
+    clock_increment = str(input("Clock Increment (in seconds):"))
+    pair_at_bool = str(input("Do you want the pairing instant (Y) or in the future (N)? Y/N"))
+    if pair_at_bool == "Y" or pair_at_bool == "y":
+        pair_in = 0
+    elif pair_at_bool == "N" or pair_at_bool == "n":
+        pair_in = int(input("In how many minutes do you want to pair the pairings?"))
+    else:
+        print("The Input was not clear. We took 10 minutes as default value!")
+        pair_in = 10
+    rated_bool = str(input("Do you want the pairing rated (Y) or unrated (N)? Y/N"))
+    if rated_bool == "Y" or rated_bool == "y":
+        rated = "true"
+    elif rated_bool == "N" or rated_bool == "n":
+        rated = "false"
+    else:
+        print("The Input was not clear. We took rated as default value!")
+        rated = "true"
+    while True:
+        print("Which message do you want to send to the players? It must contains '{game}' for the individual Game ID! When you finish press STRG+Z and Enter!")
+        message = sys.stdin.read()
+        if "{game}" in message:
+            break
+        else:
+            print("In your message is the term {game} missing. This is essential!")
+    bulk = create_games_bulk(pairings, clock_limit, clock_increment, pair_in, message, rated)
+    print(f"Your Bulk is created:\n\n{bulk.text}")
+    end = input("You can close this Window with any key.")
 
 
 def view_bulk_pairings():
@@ -90,4 +129,4 @@ def cancel_bulk(bulk_id):
 
 
 if __name__ == '__main__':
-    print("hi")
+    create_bulk()
